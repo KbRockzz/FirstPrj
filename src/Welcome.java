c470bcde99301f11b3a183abda1917b80b25e1d3
KbRockzz <23681691.thuan@student.iuh.edu.vn>
2024-08-12 18:42:45 +0700

    Adding to repo

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Nguyễn Đào Hoài Thuận");
+        System.out.println("Welcome to the DHKTPM19B!");
+    }
+}
